6e1a835b91ef3e1231275d427834bff0ef7331ef
cooms <thcoomer@hotmail.com>
2017-02-09 21:11:15 +1300

    index page fully repsonsive

--- public/jQuery.js
+++ public/jQuery.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
-	$(".titlebox").animate({marginTop: -400}, 0)
-})
+// $(document).ready(function(){
+// 	$(".titlebox").animate({marginTop: -400}, 0)
+// })
 
-$(document).ready(function(){
-	$(".titlebox").hide().fadeIn(2000)
-})
+// $(document).ready(function(){
+// 	$(".titlebox").hide().fadeIn(2000)
+// })
 
-$(document).ready(function(){
-	$(function () {
-	    $(".titlebox").animate({
-	       marginTop: 200
-	    }, { duration: 1500, queue: false });
-	})
-})
+// $(document).ready(function(){
+// 	$(function () {
+// 	    $(".titlebox").animate({
+// 	       marginTop: 200
+// 	    }, { duration: 1500, queue: false });
+// 	})
+// })
